d72a1aa4636fbcb956157d5995c635ab5d1d3a69
jonatas de oliveira souza junior <86901316+Inariano@users.noreply.github.com>
2023-09-14 12:19:39 +0000

    Update functionStart.js and index.mjs

--- src/functionStart.js
+++ src/functionStart.js
@@ -1,8 +1,28 @@
-
+export {Start}
 //Funcao iniciar a aplicacao 
 function Start ()
 {
-    ///criacao de ambiente grafico 
-    co
+    ///criacao de ambiente grafico
+     
+    ////criacao de espaco grafico principal
+ const $divmain = document.createElement("div");
+ document.body.appendChild($divmain)
+
+ ////criação de area de botoes
+ const $divbuttons = document.createElement("div");
+       $divmain.appendChild($divbuttons);
+
+ /////criacao de botao de inicio de quiz
+ const $buttonstart = document.createElement("button");
+       $buttonstart.innerText="start"
+       $divbuttons.appendChild($buttonstart)
+
+       /////chamada de questoao pelo botao
+       $buttonstart.addEventListener("click",() =>{console.log("funcao a ser chamada")}) 
+
+ 
+
+
+
 
 }
